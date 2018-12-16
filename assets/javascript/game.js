cf9791f6f659c35bf428cb172110d7380fdbd59b
xtianares <xtianares@yahoo.com>
2018-12-15 22:55:58 -0500

    finished game display, added some JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 // global variables
 let wins = 0,
-	losses = 0,
-	targetNumber = "",
+    losses = 0,
+    targetNumber = "",
     yourNumber = 0;
+
+// get random number between two numbers
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
